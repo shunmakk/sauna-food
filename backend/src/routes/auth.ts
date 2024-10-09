ec376f34170a9d5e1619cdf3ee703d75767baec9
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-10 05:29:48 +0900

    add:認証ルート

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,40 +1,50 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { PrismaClient } from ".prisma/client";
-import bcrypt from "bcrypt";
 import admin from "../config/firebaseAdmin";
 
 //認証ルート
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+// ユーザー登録エンドポイント
+router.post("/register", async (req: any, res: any) => {
+  const { email, name } = req.body;
 
   try {
-    const userRecord = await admin.auth().createUser({
-      email,
-      password,
-    });
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
+    // Prismaでユーザー情報をデータベースに保存
     const user = await prisma.user.create({
       data: {
-        // id: userRecord.uid,
-        name,
         email,
-        passwordHash,
+        name,
+        passwordHash: "", // パスワードハッシュは不要
       },
     });
 
-    res.status(201).json({ message: "ユーザーの作成に成功", userId: user.id });
+    return res.status(201).json({ message: "ユーザーが作成されました", user });
   } catch (error) {
     console.error("ユーザーの作成に失敗:", error);
-    res.status(400).json({ error: "ユーザー作成エラー" });
+    return res.status(500).json({ error: "サーバーエラー" });
   }
 });
 
-router.post("/login", async (req, res) => {
+// メールアドレスの存在チェックエンドポイント
+router.post("/check-email", async (req: any, res: any) => {
+  const { email } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (user) {
+      return res.status(200).json({ exists: true });
+    }
+    return res.status(200).json({ exists: false });
+  } catch (error) {
+    console.error("メールアドレスのチェックに失敗:", error);
+    return res.status(500).json({ error: "サーバーエラー" });
+  }
+});
+
+//ログインユーザーを登録するエンドポイント
+router.post("/login", async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   try {
@@ -43,7 +53,7 @@ router.post("/login", async (req, res) => {
 
     res.json({ token: customToken });
   } catch (error) {
-    res.status(400).json({ error: "Invalid credentials" });
+    res.status(400).json({ error: "無効な認証" });
   }
 });
 
