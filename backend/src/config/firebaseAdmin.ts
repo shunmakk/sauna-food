01d452a4fc42d44582a93e08a9bb05da2d03f178
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-09 22:21:58 +0900

    firebase adminを追加

---
+++ backend/src/config/firebaseAdmin.ts
@@ -0,0 +1,14 @@
+import admin from "firebase-admin";
+import { config } from "dotenv";
+
+config();
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
