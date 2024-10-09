c659d1b6fbbad28bfec77a35cde6938b0e26786c
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-09 22:37:05 +0900

    add:認証ミドルウェア

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import admin from "../config/firebaseAdmin";
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split("Bearer ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "トークンがありません" });
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken; // このuserはtypes/express/index.d.ts内で型定義
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "無効なトークンです" });
+  }
+};
