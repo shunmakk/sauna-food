d0646b6129c2f7642598d05f3771028cd8e1ddd4
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-10 05:46:11 +0900

    add: login

---
+++ front/src/app/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../config/firebase";
+import { useRouter } from "next/navigation";
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+export default function Login() {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const onSubmit = async (data: LoginForm) => {
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      router.push("/dashboard");
+    } catch (error) {
+      setError("ログインに失敗.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p>メールアドレス</p>
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="メールアドレス"
+          required
+        />
+        <p>パスワード</p>
+        <input
+          {...register("password")}
+          type="password"
+          placeholder="パスワード"
+          required
+        />
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
