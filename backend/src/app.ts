b988dc8bdeff81cfbf606386cfc7faf4b97e2e69
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-09 16:05:59 +0900

    add: backend構築

---
+++ backend/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+//ミドルウェア
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+//ルート
+app.get("/", (req, res) => {
+  res.json({ message: "API" });
+});
+
+//エラーハンドリング
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "500エラー" });
+  }
+);
+
+export default app;
