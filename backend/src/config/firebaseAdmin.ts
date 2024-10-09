d12cccd9a4b903ff4bbd2ecc5f2ffc5b9ec6bbc8
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-10 02:38:54 +0900

    upsate: firebase admin

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,14 +1,36 @@
 import admin from "firebase-admin";
 import { config } from "dotenv";
+import fs from "fs";
+import path from "path";
 
 config();
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
-);
+// 環境変数の設定を読み込み
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!serviceAccountPath) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY_PATHが設置されていない");
+}
+
+// フルパスの解決
+const fullPath = path.resolve(__dirname, "../../", serviceAccountPath);
+console.log("フルパスのアカウントキー:", fullPath);
+
+try {
+  const serviceAccount = JSON.parse(fs.readFileSync(fullPath, "utf8"));
+
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    });
+    console.log("FirebaseAdminの初期化に成功しました!");
+  }
+} catch (error: any) {
+  console.error(
+    "アカウントキーの読み取りまたは解析中にエラーが発生しました",
+    error.message
+  );
+  throw new Error("サービスアカウントキーのロードに失敗");
+}
 
 export default admin;
