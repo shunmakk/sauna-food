9c8344dc1c32a06e2fff21d82345e2b13e088c42
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-10 23:16:49 +0900

    fix: middlewareの型

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,9 +1,9 @@
-import { Request } from "express";
+import { DecodedIdToken } from "firebase-admin/auth";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: any;
+      user?: DecodedIdToken;
     }
   }
 }
