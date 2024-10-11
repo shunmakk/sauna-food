fc06427233fec4313034e287f40b730b0cf5fee6
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-12 04:39:06 +0900

    css周り修正

--- front/src/app/sauna-facilities/create/page.tsx
+++ front/src/app/sauna-facilities/create/page.tsx
@@ -47,42 +47,44 @@ export default function CreateSaunaFacility() {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">新規サウナ施設を追加</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <div>
-          <input
-            {...register("name", { required: "施設名は必須です" })}
-            placeholder="施設名"
-            className="w-full p-2 border rounded"
-          />
-          {errors.name && (
-            <span className="text-red-500">{errors.name.message}</span>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("address", { required: "住所は必須です" })}
-            placeholder="住所"
-            className="w-full p-2 border rounded"
-          />
-          {errors.address && (
-            <span className="text-red-500">{errors.address.message}</span>
-          )}
-        </div>
-        <div>
-          <textarea
-            {...register("description", { required: "説明は必須です" })}
-            placeholder="説明(300文字以内で入力して)"
-            className="w-full p-2 border rounded"
-            maxLength={300}
-          />
-        </div>
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-          追加
-        </button>
-      </form>
-      {error && <p className="text-red-500 mt-4">{error}</p>}
+    <div className="flex min-h-screen flex-col items-center p-8 sm:p-24 bg-gray-100">
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">新規サウナ施設を追加</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <input
+              {...register("name", { required: "施設名は必須です" })}
+              placeholder="施設名"
+              className="w-full p-2 border rounded"
+            />
+            {errors.name && (
+              <span className="text-red-500">{errors.name.message}</span>
+            )}
+          </div>
+          <div>
+            <input
+              {...register("address", { required: "住所は必須です" })}
+              placeholder="住所"
+              className="w-full p-2 border rounded"
+            />
+            {errors.address && (
+              <span className="text-red-500">{errors.address.message}</span>
+            )}
+          </div>
+          <div>
+            <textarea
+              {...register("description", { required: "説明は必須です" })}
+              placeholder="説明(300文字以内で入力して)"
+              className="w-full p-2 border rounded"
+              maxLength={300}
+            />
+          </div>
+          <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+            追加
+          </button>
+        </form>
+        {error && <p className="text-red-500 mt-4">{error}</p>}
+      </div>
     </div>
   );
 }
--- front/src/app/sauna-facilities/page.tsx
+++ front/src/app/sauna-facilities/page.tsx
@@ -30,28 +30,30 @@ export default function SaunaFacilityList() {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">サウナ施設一覧</h1>
-      <Link
-        href="/sauna-facilities/create"
-        className="bg-green-500 text-white p-2 rounded"
-      >
-        新規サウナ施設を追加
-      </Link>
-      {error && <p className="text-red-500 mt-4">{error}</p>}
-      <ul className="mt-8 space-y-2">
-        {facilities.map((facility) => (
-          <li key={facility.id} className="border p-2 rounded">
-            <Link
-              href={`/sauna-facilities/${facility.id}`}
-              className="text-blue-500 hover:underline text-2xl"
-            >
-              {facility.name}
-            </Link>
-            <div>{facility.address}</div>
-          </li>
-        ))}
-      </ul>
+    <div className="flex min-h-screen flex-col items-center p-8 sm:p-24 bg-gray-100">
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold mb-6">サウナ施設一覧</h1>
+        <Link
+          href="/sauna-facilities/create"
+          className="bg-green-500 text-white p-2 rounded"
+        >
+          新規サウナ施設を追加
+        </Link>
+        {error && <p className="text-red-500 mt-4">{error}</p>}
+        <ul className="mt-8 space-y-2">
+          {facilities.map((facility) => (
+            <li key={facility.id} className="border p-2 rounded">
+              <Link
+                href={`/sauna-facilities/${facility.id}`}
+                className="text-blue-500 hover:underline text-2xl"
+              >
+                {facility.name}
+              </Link>
+              <div>{facility.address}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
