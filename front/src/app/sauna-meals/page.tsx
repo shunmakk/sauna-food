4e3a0e6e36bd17e8b3c96b85c6433fc4ea27ab36
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-13 23:54:57 +0900

    fix: font-size微修正

--- front/src/app/sauna-meals/page.tsx
+++ front/src/app/sauna-meals/page.tsx
@@ -38,7 +38,7 @@ export default function saunaMealList() {
   return (
     <div className="flex min-h-screen flex-col items-center p-8 sm:p-24 bg-gray-100">
       <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">サウナ飯一覧</h1>
+        <h1 className="text-4xl font-bold mb-6">サウナ飯一覧</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {saunaMeal?.map((meal) => (
             <div key={meal.id} className="border p-4 rounded shadow">
