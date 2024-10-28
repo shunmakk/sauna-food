49c938a56b50d9628d2455ef0d5b63d3532e2ed6
uni <147740687+shunmakk@users.noreply.github.com>
2024-10-28 22:41:43 +0900

    検索バー、pagenationを追加

--- front/src/app/sauna-meals/page.tsx
+++ front/src/app/sauna-meals/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
 import Image from "next/image";
+import SearchBar from "@/components/common/SearchBar";
+import PageNation from "@/components/common/PageNation";
 import Header from "@/components/common/Header";
 
 interface SaunaMeal {
@@ -19,6 +21,9 @@ export default function SaunaMealList() {
   const [saunaMeal, setSaunaMeal] = useState<SaunaMeal[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [searchTerm, setSarchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [mealsPerPage] = useState(6);
 
   useEffect(() => {
     const fetchSaunaMealList = async () => {
@@ -35,6 +40,19 @@ export default function SaunaMealList() {
     fetchSaunaMealList();
   }, []);
 
+  const filterMeals = saunaMeal.filter(
+    (male) =>
+      male.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      male.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      male.price.toString().includes(searchTerm)
+  );
+
+  const indexOfLastMeal = currentPage * mealsPerPage; //最終施設のindex
+  const indexOfFirstMeal = indexOfLastMeal - mealsPerPage; //最初の施設のindex
+  const currentMeals = filterMeals.slice(indexOfFirstMeal, indexOfLastMeal); //現在の表示施設
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
@@ -43,8 +61,14 @@ export default function SaunaMealList() {
       <Header />
       <div className="container mx-auto p-4 mt-12">
         <h1 className="text-4xl font-bold mb-6">サウナ飯一覧</h1>
+        <div className="flex justify-end mb-3">
+          <SearchBar
+            setSearchTerm={setSarchTerm}
+            placeholder={"サウナ飯名を検索"}
+          />
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {saunaMeal?.map((meal) => (
+          {currentMeals?.map((meal) => (
             <div key={meal.id} className="border p-4 rounded shadow">
               <h2 className="text-xl font-semibold">{meal.name}</h2>
               {meal.imageUrl && (
@@ -71,6 +95,12 @@ export default function SaunaMealList() {
             </div>
           ))}
         </div>
+        <PageNation
+          filterContent={filterMeals}
+          PerPage={mealsPerPage}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
       </div>
     </div>
   );
