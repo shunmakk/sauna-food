04df47f5fddeb5294b2b0847be024c1f90aea8fb
uni <147740687+shunmakk@users.noreply.github.com>
2024-12-11 23:03:46 +0900

    コメントアウト消す

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -6,24 +6,6 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-[97vh] bg-gray-100 relative">
       <Header />
-      {/* <div
-        className="absolute  left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 hidden md:block"
-        style={{ top: "29%" }}
-      >
-        <div className="absolute inset-0 bg-gradient-to-r from-blue-600 to-teal-400 opacity-80 rounded-lg shadow-xl"></div>
-        <h2 className="text-3xl font-extrabold text-center text-white relative p-6 rounded-lg">
-          サウナ飯レビューアプリへようこそ
-        </h2>
-        <ul>
-          <li className="text-lg text-center text-white  mb-1 relative">
-            #自分に合ったサウナ施設、飯を探してみよう
-          </li>
-          <li className="text-lg text-center text-white  mb-2 relative">
-            #サウナ飯の投稿、レビューをしてみましょう
-          </li>
-        </ul>
-      </div> */}
-
       <div className="flex-grow container mx-auto px-4 py-8 flex flex-col items-center justify-center">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-4xl">
           <Link
