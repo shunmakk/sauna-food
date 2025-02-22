c13e80394ec0e07c39d9dd01ffb484f3b8088674
uni <147740687+shunmakk@users.noreply.github.com>
2025-02-22 21:29:54 +0900

    footerとheader

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-200">
-      <div className="p-2 text-center text-xs">フッター</div>
+      <div className="p-10 text-center text-xs">
+        <ul className="flex flex-wrap items-center justify-center gap-5">
+          <li className="hover:text-gray-400">
+            <Link href="/sauna-facilities">ホーム</Link>
+          </li>
+          <li className="hover:text-gray-400">
+            <Link href="/sauna-facilities">サウナ施設一覧</Link>
+          </li>
+          <li className="hover:text-gray-400">
+            <Link href="/sauna-meals">サウナ飯一覧</Link>
+          </li>
+          <li className="hover:text-gray-400">
+            <Link href="/sauna-facilities/create">サウナ施設追加</Link>
+          </li>
+          <li className="hover:text-gray-400">
+            <Link href="/profile">プロフィール</Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
