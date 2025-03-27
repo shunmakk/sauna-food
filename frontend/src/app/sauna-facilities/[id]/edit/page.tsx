409294e41cf3cfc25e4c8194babae630c47db864
uni <147740687+shunmakk@users.noreply.github.com>
2025-03-28 00:58:10 +0900

    fix:編集 pageのRHF周りの修正、add: Alert表示

--- frontend/src/app/sauna-facilities/[id]/edit/page.tsx
+++ frontend/src/app/sauna-facilities/[id]/edit/page.tsx
@@ -5,6 +5,10 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../../../../../context/AuthContext";
+import { Alert } from "@mui/material";
+import AlertTitle from "@mui/material/AlertTitle";
+import Snackbar from "@mui/material/Snackbar";
+import ClearIcon from "@mui/icons-material/Clear";
 
 interface SaunaFacilityForm {
   name: string;
@@ -21,11 +25,15 @@ export default function EditSaunaFacility({
     register,
     handleSubmit,
     setValue,
-    formState: { errors },
+    formState: { isDirty, errors },
   } = useForm<SaunaFacilityForm>();
   const [error, setError] = useState("");
+  //faclitiyNameを取得
+  const [getFaclityName, setgetFaclityName] = useState<string>("");
   const router = useRouter();
   const { user } = useAuth();
+
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const fetchFaclity = async () => {
@@ -34,9 +42,10 @@ export default function EditSaunaFacility({
           `http://localhost:5000/api/sauna-facilities/${params.id}`
         );
         const faclity = res.data;
-        setValue("name", faclity?.name);
+        setgetFaclityName(faclity?.name);
+
         setValue("address", faclity?.address);
-        setValue("description", faclity?.escription);
+        setValue("description", faclity?.description);
       } catch (error) {
         console.error("サウナ施設取得エラー:", error);
         setError("サウナ施設の取得に失敗しました");
@@ -47,7 +56,7 @@ export default function EditSaunaFacility({
 
   const onSubmit = async (data: SaunaFacilityForm) => {
     if (!user) {
-      setError("編集するにはログインして！");
+      setError("編集するにはログインする必要があります");
       return;
     }
 
@@ -61,40 +70,86 @@ export default function EditSaunaFacility({
           },
         }
       );
-      router.push("/");
+      router.push(`/sauna-facilities/${params.id}`);
     } catch (error) {
       console.error("編集エラー", error);
       setError("編集に失敗しました");
     }
   };
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = (event: React.MouseEvent<HTMLDivElement>) => {
+    setOpen(false);
+  };
+
   return (
     <>
       <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4">サウナ施設を編集</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <h1 className="text-2xl font-bold mb-4">{getFaclityName}を編集</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 lg:w-3/4">
           <div>
+            <label htmlFor="address">住所</label>
             <input
               {...register("address", { required: "住所は必須です" })}
+              id="adress"
+              type="text"
               placeholder="住所"
               className="w-full p-2 border rounded"
             />
             {errors.address && (
-              <span className="text-red-500">{errors.address.message}</span>
+              <span className="text-red-500">{errors?.address?.message}</span>
             )}
           </div>
           <div>
+            <label htmlFor="description">説明</label>
             <textarea
-              {...register("description")}
-              placeholder="説明"
-              className="w-full p-2 border rounded"
+              {...register("description", { required: "説明の入力は必須です" })}
+              placeholder="説明(300文字以内で入力してください)"
+              className="w-full  p-2 border roundedresize-y min-h-[100px] h-[250px] lg:h-[200px]"
+              maxLength={300}
             />
+            {errors.description && (
+              <span className="text-red-500">
+                {errors?.description?.message}
+              </span>
+            )}
           </div>
-          <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded"
+            disabled={!isDirty}
+            onClick={handleOpen}
+          >
             更新
           </button>
         </form>
-        {error && <p className="text-red-500 mt-4">{error}</p>}
+        {error && (
+          // ここは他の画面でも共通化してもいいかも
+          <Snackbar
+            open={open}
+            onClose={(event) =>
+              handleClose(event as React.MouseEvent<HTMLDivElement>)
+            }
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          >
+            <Alert
+              severity="error"
+              action={
+                <div
+                  onClick={(event) => handleClose(event)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <ClearIcon />
+                </div>
+              }
+            >
+              <AlertTitle>Error</AlertTitle>
+              {error}
+            </Alert>
+          </Snackbar>
+        )}
       </div>
     </>
   );
---
+++ frontend/src/components/common/errorAlert.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const errorAlert = () => {
+  return;
+  //   後々エラーアラートを共通化
+  <div></div>;
+};
+
+export default errorAlert;
